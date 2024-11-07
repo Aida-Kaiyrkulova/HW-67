f62a017d21c84c4277432b401e216e60f6cfaec7
Aida-Kaiyrkulova <aida-kaiyr@yandex.ru>
2024-11-07 16:28:31 +0600

    Create store.ts for setting Redux store

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { codeReducer } from '../containers/Code/codeSlice.ts';
+
+export const store = configureStore({
+  reducer:{
+    code: codeReducer,
+  },
+});
+
+ export type RootState = ReturnType<typeof store.getState>;
+ export type AddDispatch = typeof store.dispatch;
